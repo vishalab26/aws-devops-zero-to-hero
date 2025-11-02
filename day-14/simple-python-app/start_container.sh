59d467942cbf722e398aa1ba4e3fb0a59885a22e
vishalab26 <vishalmath2605@gmail.com>
2025-11-02 17:16:07 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull vishalmath2605/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 vishalmath2605/simple-python-flask-app
